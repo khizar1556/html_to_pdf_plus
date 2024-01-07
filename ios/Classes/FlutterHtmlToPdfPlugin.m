349eb353294bccb5832e6d19774cec3747aaa7cf
Khizar Rehman <ranakhizar1556@gmail.com>
2024-01-07 21:02:55 +0500

    add ios support

--- ios/Classes/FlutterHtmlToPdfPlugin.m
+++ ios/Classes/FlutterHtmlToPdfPlugin.m
@@ -1,11 +1,11 @@
 #import "FlutterHtmlToPdfPlugin.h"
-#if __has_include(<flutter_html_to_pdf/flutter_html_to_pdf-Swift.h>)
-#import <flutter_html_to_pdf/flutter_html_to_pdf-Swift.h>
+#if __has_include(<html_to_pdf_plus/html_to_pdf_plus-Swift.h>)
+#import <html_to_pdf_plus/html_to_pdf_plus-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_html_to_pdf-Swift.h"
+#import "html_to_pdf_plus-Swift.h"
 #endif
 
 @implementation FlutterHtmlToPdfPlugin
